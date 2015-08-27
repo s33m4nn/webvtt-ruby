c5e289721d9f3e1173fb35c4b7a09c8a29225f70
Gábor Zéman <zeman.gabor@gmail.com>
2015-08-27 18:00:46 +0200

    Add ability to parse vtt with missing hours part of timestamp. The hour part is optional by vtt specification.

--- lib/parser.rb
+++ lib/parser.rb
@@ -142,7 +142,7 @@ module WebVTT
         lines.shift
       end
 
-      if lines[0].match(/([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}) -+> ([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3})(.*)/)
+      if lines[0].match(/((?:[0-9]{2}:)?[0-9]{2}:[0-9]{2}\.[0-9]{3}) -+> ((?:[0-9]{2}:)?[0-9]{2}:[0-9]{2}\.[0-9]{3})(.*)/)
         @start = Timestamp.new $1
         @end = Timestamp.new $2
         @style = Hash[$3.strip.split(" ").map{|s| s.split(":").map(&:strip) }]
